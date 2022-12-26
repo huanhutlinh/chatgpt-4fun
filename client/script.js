38a36a85490bbcada3587c9f7fceaf6eb0dbbda6
huanhutlinh <huanhutlinh128@gmail.com>
2022-12-27 00:35:16 +0700

    Use Own BE

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://ai-chat.onrender.com', {
+    const response = await fetch('https://linhhua-chatgpt-4fun.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
